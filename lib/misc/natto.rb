b21120fae68267f209650da7cc84689790c0cff0
exabugs <exabugs@gmail.com>
2013-12-19 13:04:53 +0900

    modified template.rb.

--- lib/misc/natto.rb
+++ lib/misc/natto.rb
@@ -3,7 +3,7 @@ require 'nkf'
 module Misc
   class Natto
 
-    def tf(text, nbest = 2)
+    def self.tf(text, nbest = 2)
 
       # 英数字 全角->半角, カタカナ 半角->全角
       text = NKF.nkf('-m0Z1 -w', text)
@@ -18,10 +18,10 @@ module Misc
 
       array.each do |tgt|
         mecab.parse(tgt) do |n|
-          #puts "#{n.surface}\t#{n.feature}"
+          puts "#{n.surface}\t#{n.feature}"
           info = n.feature.force_encoding("UTF-8").split(",")
           if check_0[info[0]] && check_1[info[1]]
-            #puts "ーーーーーーーーーーーーーーーーーーーーー #{n.surface}\t#{n.feature}"
+            puts "ーーーーーーーーーーーーーーーーーーーーー #{n.surface}\t#{n.feature}"
             word = n.surface.force_encoding("UTF-8")
             word.downcase!
             terms[word] += 1
@@ -38,11 +38,55 @@ module Misc
 
     end
 
-    def condition(text)
+    def self.to_tf_hash(text)
       tf(text,1).inject({}) do |ret, a|
         ret[a[:k]] = a[:w]
         ret
       end
+    end
+
+    def self.search(collection, text)
+      condition = to_tf_hash(text)
+      collection.where("tf.v.k" => { "$all" => condition.keys})
+    end
+
+    def self.similar_search(collection, text)
+      condition = to_tf_hash(text)
+
+      map = %Q|
+        function() {
+          var sum = 0;
+          var a = this.tf.v;
+          for (var i = 0; i < a.length; i++) {
+            var info = a[i];
+            if (condition[info.k]) {
+              sum += info.w * condition[info.k];
+            }
+          }
+          if (0 < sum) emit(this._id, sum / this.tf.l);
+        }
+      |
+
+      reduce = %Q|
+        function(key, values) {
+          return values[0];
+        }
+      |
+
+      result = collection.where("tf.v.k" => {"$in" => condition.keys}).map_reduce(map, reduce)
+        .out(inline: true).scope(condition: condition)
+
+      result = result.sort{|x, y| y["value"] <=> x["value"] }
+
+      result.inject([]) do |ret, t|
+        object = collection.find(t["_id"])
+        if (object)
+          object.similarity = t["value"]
+          ret << object 
+        end
+        ret
+      end
+
     end
 
   end
